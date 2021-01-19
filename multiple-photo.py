3a185979029eaeb879658b38a734472b5955403c
anika <al8@wellesley.edu>
2021-01-19 15:17:45 -0500

    1/19/21

--- multiple-photo.py
+++ multiple-photo.py
@@ -1,14 +1,15 @@
 from picamera import PiCamera
 from time import sleep
 import os
+from datetime import datetime
 
 camera = PiCamera()
 camera.rotation = 270
 
-folder = os.mkdir('/home/pi/Desktop/test')
+folder = os.mkdir('/home/anika/Desktop/test')
 
 camera.start_preview(alpha=100)
 for i in range(5):
     sleep(5)
-    camera.capture('/home/pi/Desktop/test/image%s.jpg' % (i+1))
+    camera.capture('/home/anika/Desktop/test/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.jpg')
 camera.stop_preview()
--- single-photo.py
+++ single-photo.py
@@ -1,12 +1,11 @@
 from picamera import PiCamera
 from time import sleep
-import time
+from datetime import datetime
 
 camera = PiCamera()
 camera.rotation = 270
 
-
 camera.start_preview(alpha=100)
 sleep(5)
-camera.capture('/home/pi/Desktop/image.jpg')
+camera.capture('/home/anika/Desktop/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.jpg')
 camera.stop_preview()

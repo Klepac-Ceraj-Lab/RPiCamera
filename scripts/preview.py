54901c6932ad376298f769a5c535762b3fde9342
al82023 <al8@wellesley.edu>
2021-07-19 19:34:47 -0400

    delete alpha from preview

--- scripts/preview.py
+++ scripts/preview.py
@@ -3,6 +3,6 @@ from time import sleep
 
 camera = PiCamera()
 
-camera.start_preview(alpha=100) # alpha value makes preview slightly see-through so screen is still visible
+camera.start_preview()
 sleep(60) # previews for 60sec
 camera.stop_preview()

b85dcc6f3d01466ba69145ffe48f6d9cee9c3385
Anika Luo <al8@wellesley.edu>
2021-12-09 13:15:14 -0500

    add command line arg

--- scripts/video.py
+++ scripts/video.py
@@ -4,8 +4,13 @@ from datetime import datetime
 
 camera = PiCamera()
 
+# takes command line argument
+parser = argparse.ArgumentParser()
+parser.add_argument("dir")
+args = parser.parse_args()
+
 camera.start_preview(alpha=100) # alpha value makes preview slightly see-through so screen is still visible
-camera.start_recording('/home/anika/Desktop/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.h264')
+camera.start_recording(str(args.dir) + datetime.now().strftime("%Y%m%d-%H%M%S") + '.h264') # takes video and saves to (directory given in command line)/timestamp.h264
 sleep(5) # takes video for 5s
 camera.stop_recording()
 camera.stop_preview()

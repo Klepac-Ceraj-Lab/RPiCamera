65134671829f30e9cd04b2a48ee04ac04e85dbd5
Anika Luo <al8@wellesley.edu>
2021-12-09 13:23:50 -0500

    video

--- scripts/video.py
+++ scripts/video.py
@@ -7,11 +7,12 @@ camera = PiCamera()
 
 # takes command line argument
 parser = argparse.ArgumentParser()
-parser.add_argument("dir")
+parser.add_argument("dir") # first argument = destination directory
+parser.add_argument("time") # second argument = video time
 args = parser.parse_args()
 
 camera.start_preview(alpha=100) # alpha value makes preview slightly see-through so screen is still visible
 camera.start_recording(str(args.dir) + datetime.now().strftime("%Y%m%d-%H%M%S") + '.h264') # takes video and saves to (directory given in command line)/timestamp.h264
-sleep(5) # takes video for 5s
+sleep(args.time) # takes video for 5s
 camera.stop_recording()
 camera.stop_preview()

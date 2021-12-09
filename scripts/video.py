66c3029836279e3c54c5af821ea858947cb685af
Anika Luo <al8@wellesley.edu>
2021-12-09 13:25:29 -0500

    video

--- scripts/video.py
+++ scripts/video.py
@@ -11,8 +11,11 @@ parser.add_argument("dir") # first argument = destination directory
 parser.add_argument("time") # second argument = video time
 args = parser.parse_args()
 
-camera.start_preview(alpha=100) # alpha value makes preview slightly see-through so screen is still visible
-camera.start_recording(str(args.dir) + datetime.now().strftime("%Y%m%d-%H%M%S") + '.h264') # takes video and saves to (directory given in command line)/timestamp.h264
-sleep(args.time) # takes video for 5s
-camera.stop_recording()
-camera.stop_preview()
+println(args.dir)
+println(args.time)
+
+# camera.start_preview(alpha=100) # alpha value makes preview slightly see-through so screen is still visible
+# camera.start_recording(str(args.dir) + datetime.now().strftime("%Y%m%d-%H%M%S") + '.h264') # takes video and saves to (directory given in command line)/timestamp.h264
+# sleep(args.time) # takes video for 5s
+# camera.stop_recording()
+# camera.stop_preview()

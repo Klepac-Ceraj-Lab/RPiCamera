93da8bfc14a085f53145610bdfaf90635d4eb0fb
Anika Luo <al8@wellesley.edu>
2021-12-09 13:03:45 -0500

    readd video.py

---
+++ scripts/video.py
@@ -0,0 +1,11 @@
+from picamera import PiCamera
+from time import sleep
+from datetime import datetime
+
+camera = PiCamera()
+
+camera.start_preview(alpha=100)
+camera.start_recording('/home/anika/Pictures/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.h264')
+sleep(5)
+camera.stop_recording()
+camera.stop_preview()

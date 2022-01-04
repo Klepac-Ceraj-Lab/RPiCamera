61eed911ed5c9722d9dfb0f611da0e810088dfb4
al82023 <al8@wellesley.edu>
2022-01-04 17:04:12 -0500

    change remotepreview to take dir

--- scripts/remotepreview.py
+++ scripts/remotepreview.py
@@ -6,10 +6,10 @@ camera = PiCamera()
 
 # takes command line argument
 parser = argparse.ArgumentParser()
-parser.add_argument("path")
+parser.add_argument("dir")
 args = parser.parse_args()
 
 camera.start_preview(alpha=100) # alpha value makes preview slightly see-through so screen is still visible
 sleep(5) # previews for 5sec before taking photo
-camera.capture(str(args.path)) # takes photo and saves to directory and filename given in command line
+camera.capture(str(args.dir) + 'remotepreview.jpg') # takes photo and saves to <destination directory>/remotepreview.jpg
 camera.stop_preview()
